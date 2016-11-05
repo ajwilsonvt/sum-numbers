e2e3d3f383103b646b7fe059dd41a3000258c227
ajwilsonvt <ajwilsonvt@users.noreply.github.com>
2016-11-04 21:25:53 -0400

    added max values

--- src/sumnumbers/SumNumbers.java
+++ src/sumnumbers/SumNumbers.java
@@ -3,7 +3,12 @@ package sumnumbers;
 import java.util.*;
 
 /**
- * Sums integers from 1 to n.
+ * Sums integers from 1 to n. Uses long for calculation and
+ * casts result as an integer. Solved result = (x*(x+1))/2
+ * formula to derive that 65,535 is the max value of x that
+ * can return a result within java int max value of
+ * 2,147,483,647. -2,147,483,647 is min, but negative integers
+ * are prohibited in this program.
  * 
  * @author	Aaron
  * @date	2016.04.18
@@ -26,18 +31,26 @@ public class SumNumbers {
 		Scanner input = new Scanner(System.in);
 		
 		while (true) {
-			System.out.print("Enter integer: ");
-			if (input.hasNextInt()) {
-				int number = input.nextInt();
-				int result = (number*(number+1)) / 2;
-				System.out.println("Answer: "+result);
-				break;
+			System.out.print("Enter whole number [0, 65535]: ");
+			if (input.hasNextLong()) {
+				long number = input.nextLong();
+				
+				if (0 <= number && number <= 65535) {
+					long result = (number*(number+1)) / 2;
+					System.out.println("Answer: "+(int)result);
+					break;
+				}
+				else {
+					System.out.println("Number outside of range");
+					continue;
+				}
 			}
 			else {
-				input.next();
 				System.out.println("Try again");
+				input.next();
 			}
 		}
+		
 		input.close();
 	}
 	
